12b76539ca8f39305be9ff712be12d8c4b588f39
Giada Sacchi <s2122634@ed.ac.uk>
2020-10-08 13:47:16 +0100

    Created document for extra exercises

---
+++ BinProbExtended.py
@@ -0,0 +1,14 @@
+# Write a function which takes a list of items and a bin size as an input,
+# and returns a list of lists as an output, where each sub-list represents the
+# contents of one bin, as a list of items in that bin (as opposed to simply
+# returning one number which is the total volume occupied in that bin).
+# For example, for 2 open bins, your output could look like
+# [[10, 11, 4], [11, 12, 2]], to indicate which objects are in each bin
+# (which would have been [25, 24] in the video example).
+
+
+
+
+# Try some of the other heuristics for the bin-packing problem. You could
+# compare the packings you obtain with different methods, or use functions from
+# the time module to compare their speed of execution.
